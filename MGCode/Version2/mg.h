ab483ae29a7e6607c0e9d32264a93f1d7ec0c69e
tavoGFC <gusfcarrera@hotmail.es>
2017-06-14 13:29:40 -0600

    Add files via upload

---
+++ MGCode/Version2/mg.h
@@ -0,0 +1,30 @@
+#define SET 1
+#define DEF 2
+#define PRINCIPAL 3
+#define LOOP 4
+#define INILOOP 5
+#define FINLOOP 6
+#define BLAK 7
+#define NEG 8
+#define CORCH 9
+#define SCORCH 10       
+#define INICOMP 11
+#define FINCOMP 12
+#define INIMUSICA 13
+#define FINMUSICA 14
+#define ENCASO 15
+#define CONTRARIO 16
+#define FINENCASO 17
+#define EJECUTE 18
+#define SILENC 19
+#define ENTONCES 20
+#define INICIO 21
+#define FINAL 22
+#define NOTE 23
+#define FUNC 24
+#define IDENTIFIER 25
+#define NUMBER 26
+
+
+
+
